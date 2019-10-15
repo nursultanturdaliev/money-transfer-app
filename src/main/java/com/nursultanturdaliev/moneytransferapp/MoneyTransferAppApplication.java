def42209a3681000513173515ccf55d904b10ac3
Nursultan Turdaliev <nursultan.turdaliev@aboutyou.de>
2019-10-15 13:45:13 -0500

    Controller Level Exception Example

--- src/main/java/com/nursultanturdaliev/moneytransferapp/MoneyTransferAppApplication.java
+++ src/main/java/com/nursultanturdaliev/moneytransferapp/MoneyTransferAppApplication.java
@@ -6,10 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
 @SpringBootApplication
-//@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class MoneyTransferAppApplication {
-
-    public static final String RECORD_CONFLICT = "RECORD_CONFLICT";
 
     public static void main(String[] args) {
         SpringApplication.run(MoneyTransferAppApplication.class, args);
