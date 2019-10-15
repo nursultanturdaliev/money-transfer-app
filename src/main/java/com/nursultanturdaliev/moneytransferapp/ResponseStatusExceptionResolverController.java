fdcebd27240cc673d7b4cb51a432f481633bdcbf
Nursultan Turdaliev <nursultan.turdaliev@aboutyou.de>
2019-10-15 14:23:21 -0500

    add HandlerExceptionResolver: ResponseStatusExceptionResolver

---
+++ src/main/java/com/nursultanturdaliev/moneytransferapp/ResponseStatusExceptionResolverController.java
@@ -0,0 +1,16 @@
+package com.nursultanturdaliev.moneytransferapp;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/response-status-exception-resolver")
+@Controller
+public class ResponseStatusExceptionResolverController {
+
+    @GetMapping("/")
+    public ResponseEntity<Object> fetchAll() throws UserResourceNotFoundException {
+        throw new UserResourceNotFoundException();
+    }
+}
---
+++ src/main/java/com/nursultanturdaliev/moneytransferapp/UserResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.nursultanturdaliev.moneytransferapp;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT)
+public class UserResourceNotFoundException extends Exception {
+    public UserResourceNotFoundException() {
+        super();
+    }
+}
