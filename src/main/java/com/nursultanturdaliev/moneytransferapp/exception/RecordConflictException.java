8db23eea184c1d098578647b0f6babaf2e14646c
Nursultan Turdaliev <nursultan.turdaliev@aboutyou.de>
2019-10-17 13:00:29 -0500

    remove unnecessary code

--- src/main/java/com/nursultanturdaliev/moneytransferapp/exception/RecordConflictException.java
+++
@@ -1,7 +0,0 @@
-package com.nursultanturdaliev.moneytransferapp.exception;
-
-public class RecordConflictException extends RuntimeException {
-    public RecordConflictException(String s) {
-        super(s);
-    }
-}
