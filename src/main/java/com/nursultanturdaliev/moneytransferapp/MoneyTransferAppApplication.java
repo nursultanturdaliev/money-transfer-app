8f6d49174471b462288234a50131342516e14045
Nursultan Turdaliev <nursultan.turdaliev@aboutyou.de>
2019-10-17 13:16:02 -0500

    remove all swagger related codes

--- src/main/java/com/nursultanturdaliev/moneytransferapp/MoneyTransferAppApplication.java
+++ src/main/java/com/nursultanturdaliev/moneytransferapp/MoneyTransferAppApplication.java
@@ -2,68 +2,14 @@ package com.nursultanturdaliev.moneytransferapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMethod;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.Collections;
-
-import static com.google.common.collect.Lists.newArrayList;
 
 @SpringBootApplication
-@EnableSwagger2
 public class MoneyTransferAppApplication {
 
     public static final String RECORD_CONFLICT = "RECORD_CONFLICT_CONSTANT";
 
     public static void main(String[] args) {
         SpringApplication.run(MoneyTransferAppApplication.class, args);
-    }
-
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo())
-                .useDefaultResponseMessages(false)
-                .globalResponseMessage(RequestMethod.GET,
-                        newArrayList(new ResponseMessageBuilder()
-                                        .code(500)
-                                        .message("500 message")
-                                        .responseModel(new ModelRef("Error"))
-                                        .build(),
-                                new ResponseMessageBuilder()
-                                        .code(403)
-                                        .message("Forbidden!")
-                                        .build(),
-                                new ResponseMessageBuilder()
-                                        .code(404)
-                                        .message("Not Found!")
-                                        .build()));
-    }
-
-    private ApiInfo apiInfo() {
-        return new ApiInfo(
-                "Money Transfer App REST API",
-                "This is the official REST API documentation",
-                "v1.0.0",
-                "Terms of Service",
-                new Contact("Nursultan Turdaliev", "kapusta.com", "contact@kapusta.com"),
-                "License of API",
-                "API License URL",
-                Collections.emptyList()
-        );
     }
 
 }
