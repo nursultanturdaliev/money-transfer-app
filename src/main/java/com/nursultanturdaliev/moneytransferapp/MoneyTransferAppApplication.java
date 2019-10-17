3d704d086a028b092190d115e36ec618f8019f54
Nursultan Turdaliev <nursultan.turdaliev@aboutyou.de>
2019-10-17 15:09:40 -0500

    TENTH STEP: Custom Methods Response Messages - globally overriding response messages of HTTP methods

--- src/main/java/com/nursultanturdaliev/moneytransferapp/MoneyTransferAppApplication.java
+++ src/main/java/com/nursultanturdaliev/moneytransferapp/MoneyTransferAppApplication.java
@@ -3,8 +3,10 @@ package com.nursultanturdaliev.moneytransferapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
@@ -12,6 +14,8 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.Collections;
+
+import static com.google.common.collect.Lists.newArrayList;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -24,6 +28,15 @@ public class MoneyTransferAppApplication {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .globalResponseMessage(RequestMethod.GET,
+                newArrayList(new ResponseMessageBuilder()
+                                .code(200)
+                                .message("successful request")
+                                .build(),
+                        new ResponseMessageBuilder()
+                                .code(404)
+                                .message("resource not found")
+                                .build()))
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/api/users/*"))
