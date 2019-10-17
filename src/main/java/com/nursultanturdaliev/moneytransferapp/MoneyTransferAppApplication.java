cfa6013c92c8883ac94a28c2fba411b329410847
Nursultan Turdaliev <nursultan.turdaliev@aboutyou.de>
2019-10-17 14:56:08 -0500

    NINTH STEP: Custom Information - changind default api information with ApiInfo.

--- src/main/java/com/nursultanturdaliev/moneytransferapp/MoneyTransferAppApplication.java
+++ src/main/java/com/nursultanturdaliev/moneytransferapp/MoneyTransferAppApplication.java
@@ -1,15 +1,17 @@
 package com.nursultanturdaliev.moneytransferapp;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -25,6 +27,17 @@ public class MoneyTransferAppApplication {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/api/users/*"))
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "MONEY TRANSFER APP REST API",
+                "This is the main rest api documentation.",
+                "API V1.0.0",
+                "Terms of service",
+                new Contact("Nursultan Turdaliev", "www.kapusta.com", "contact@kapusta.com"),
+                "GPL 3.0", "https://www.gnu.org/licenses/gpl-3.0.en.html", Collections.emptyList());
     }
 }
