9b8cea77ed2d38a9a8770cfb3e4e58c24e53430a
Nursultan Turdaliev <nursultan.turdaliev@aboutyou.de>
2019-10-16 21:34:25 -0500

    add api meta info

--- src/main/java/com/nursultanturdaliev/moneytransferapp/MoneyTransferAppApplication.java
+++ src/main/java/com/nursultanturdaliev/moneytransferapp/MoneyTransferAppApplication.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @SpringBootApplication
 @EnableSwagger2
@@ -25,7 +29,21 @@ public class MoneyTransferAppApplication {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Money Transfer App REST API",
+                "This is the official REST API documentation",
+                "v1.0.0",
+                "Terms of Service",
+                new Contact("Nursultan Turdaliev", "kapusta.com", "contact@kapusta.com"),
+                "License of API",
+                "API License URL",
+                Collections.emptyList()
+        );
     }
 
 }
