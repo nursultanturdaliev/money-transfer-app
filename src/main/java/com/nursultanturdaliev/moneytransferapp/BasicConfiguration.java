72375537bfe0699f4e64117e4c8a93925cedb777
Nursultan Turdaliev <nursultan.turdaliev@aboutyou.de>
2019-10-19 01:19:35 -0500

    SECURITY 6: changing password encoders

--- src/main/java/com/nursultanturdaliev/moneytransferapp/BasicConfiguration.java
+++ src/main/java/com/nursultanturdaliev/moneytransferapp/BasicConfiguration.java
@@ -14,7 +14,7 @@ public class BasicConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("user").password("{noop}password").roles("USER")
+                .withUser("user").password("{bcrypt}$2a$10$dXJ3SW6G7P50lGmMkkmwe.20cQQubK3.HZWzG3YB1tlRy.fqvM/BG").roles("USER")
                 .and()
                 .withUser("admin").password("{noop}password").roles("USER", "ADMIN");
     }
