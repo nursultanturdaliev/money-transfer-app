23c97fe20e04c623ffb899ccfdaf25ee89fd021f
Nursultan Turdaliev <nursultan.turdaliev@aboutyou.de>
2019-10-19 00:08:07 -0500

    SECURITY 4: configuring spring boot security. basic authentication + in memory.

---
+++ src/main/java/com/nursultanturdaliev/moneytransferapp/BasicConfiguration.java
@@ -0,0 +1,31 @@
+package com.nursultanturdaliev.moneytransferapp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class BasicConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("{noop}password").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}password").roles("USER", "ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
