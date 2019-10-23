be76e981d98dc6cd96178512cba85e8b58c346df
Nursultan Turdaliev <nursultan.turdaliev@aboutyou.de>
2019-10-22 19:03:42 -0500

    remove unnecessary code

--- src/main/java/com/nursultanturdaliev/moneytransferapp/controller/TransactionController.java
+++ src/main/java/com/nursultanturdaliev/moneytransferapp/controller/TransactionController.java
@@ -9,10 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 
 @Controller
@@ -43,6 +40,7 @@ public class TransactionController {
 
     }
 
+    @PostMapping("/")
     public ResponseEntity<Transaction> create(@RequestBody TransactionDto transactionDto) {
         Transaction transaction = transactionService.createTransaction(transactionDto);
 
--- src/main/java/com/nursultanturdaliev/moneytransferapp/repository/CurrencyRepository.java
+++ src/main/java/com/nursultanturdaliev/moneytransferapp/repository/CurrencyRepository.java
@@ -5,6 +5,5 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CurrencyRepository extends CrudRepository<Currency, Integer > {
-    @Query("Select * from currencies where name='USD' limit 1")
     Currency findOneByName(String currencyCode);
 }
