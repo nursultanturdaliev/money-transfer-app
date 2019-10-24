6674207062a742ee3596452019b6e2ceec26d5ad
Nursultan Turdaliev <nursultan.turdaliev@aboutyou.de>
2019-10-24 12:36:48 -0500

    rework login and registration pages

--- src/main/java/com/nursultanturdaliev/moneytransferapp/auth/UserDetailsServiceImpl.java
+++ src/main/java/com/nursultanturdaliev/moneytransferapp/auth/UserDetailsServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -21,6 +22,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     UserRepository userRepository;
 
     @Override
+    @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         User user = userRepository.findOneByUsername(username);
--- src/main/java/com/nursultanturdaliev/moneytransferapp/controller/RegistrationController.java
+++ src/main/java/com/nursultanturdaliev/moneytransferapp/controller/RegistrationController.java
@@ -14,7 +14,6 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class RegistrationController {
@@ -57,12 +56,6 @@ public class RegistrationController {
 
     @GetMapping("/login")
     public String login(Model model, String error, String logout) {
-        if (error != null)
-            model.addAttribute("error", "Your username and password is invalid.");
-
-        if (logout != null)
-            model.addAttribute("message", "You have been logged out successfully.");
-
         return "login";
     }
 }
